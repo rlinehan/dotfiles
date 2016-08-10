1999d45b594ebfcb5a1798c80e4bfca1d7d5b560
Ruth Linehan <ruth@puppetlabs.com>
2016-08-10 11:42:43 -0700

    Add ruth plugin to lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,10 @@
-{:user {:plugins [[venantius/ultra "0.3.4"]]
+{:user {:plugins [
+                  ;[pjstadig/humane-test-output "0.7.1"]
+                  ;[venantius/ultra "0.3.4" :exclusions [org.clojure/clojure]]
+                  ;[lein-difftest "2.0.0"]
+                  ]
         :ultra  {:color-scheme :solarized_dark}
         :whidbey  {:width 80}
         :dependencies [;[xrepl "0.1.0"]
-                       ]}}
+                       ] }
+  :ruth {:plugins [[venantius/ultra "0.3.4" :exclusions [org.clojure/clojure]]]}}
