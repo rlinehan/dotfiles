1ca932b36c3a1ef82286ec25d3b2d70f8973d529
Ruth Linehan <ruth@puppetlabs.com>
2017-06-16 16:21:18 -0700

    all the things

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,5 +6,7 @@
         :ultra  {:color-scheme :solarized_dark}
         :whidbey  {:width 80}
         :dependencies [;[xrepl "0.1.0"]
-                       ] }
+                       ]
+        :puppetserver-heap-size "5G"
+        }
   :ruth {:plugins [[venantius/ultra "0.3.4" :exclusions [org.clojure/clojure]]]}}
