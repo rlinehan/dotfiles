5c85cb2d5748d0bead03be04dbdb9b579cf47ecd
Ruth Linehan <rlinehan@github.com>
2021-08-25 17:22:49 -0700

    Update symlink.sh
    Follow bash conventions better, add handling for .config/ directory

--- symlink.sh
+++ symlink.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-DIR=`pwd`
+DIR=$(pwd)
 
-FILES=`ls -a | grep -v symlink | egrep -v '.git$' | egrep -v '^\.+$' | grep -v README`
+# list all files in this directory excluding the README, symlink.sh, and
+# script, .git, and .config dirs
+FILES=$(find -s . -maxdepth 1  ! -name "README.md" ! -name "symlink.sh" ! -name "script" ! -name ".config" ! -name ".git" ! -name "." | sed "s/\.\///")
 
-for f in $FILES
+# list files in .config/, excluding .config itself
+# this is separated out because unlike .vim/ I don't want to completely clobber
+# ~/.config
+CONFIG_DIR_FILES=$(find -s .config -maxdepth 1 ! -name ".config" -print0)
+
+for f in $FILES $CONFIG_DIR_FILES
 do
-  if [ -e ~/$f ]
-  then
-    echo ~/$f already exists
-  else
-    ln -s $DIR/$f $HOME/$f
-    echo symlink created from $DIR/$f to ~/$f
-  fi
+ if [ -e "$HOME/$f" ]
+ then
+   echo "- $HOME/$f already exists"
+ else
+   ln -s "$DIR/$f" "$HOME/$f"
+   echo "* symlink created from $DIR/$f to $HOME/$f"
+ fi
 done
